6322be5b17f48775f5e77e0d40024eeef9f59dea
Michael Dumfart <michi.dumfart@gmail.com>
2022-07-15 12:34:12 +0200

    Add remote pull and compose

--- src/saft/src/main/java/net/sternstein/saft/ExampleResource.java
+++ src/saft/src/main/java/net/sternstein/saft/ExampleResource.java
@@ -11,6 +11,6 @@ public class ExampleResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello from RESTEasy Reactive";
+        return "Hello from RESTEasy Reactive!";
     }
 }
