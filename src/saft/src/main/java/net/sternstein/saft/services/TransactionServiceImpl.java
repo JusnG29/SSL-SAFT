cdd6efa69e6b9b461ee5c06dbb7a03ce86c184ee
JusnG29 <juliangartner@gmail.com>
2022-07-18 17:34:25 +0200

    created interfaces and implementation for all entities (service-layer)

---
+++ src/saft/src/main/java/net/sternstein/saft/services/TransactionServiceImpl.java
@@ -0,0 +1,8 @@
+package net.sternstein.saft.services;
+
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class TransactionServiceImpl implements TransactionService {
+
+}
