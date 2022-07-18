0bbfeb859b292b54b5d5247ae200d49d16a61af0
JusnG29 <juliangartner@gmail.com>
2022-07-18 17:34:53 +0200

    add basic annotations to rest impl

--- src/saft-backend/src/main/java/net/sternstein/saft/api/ProductRestApi.java
+++ src/saft-backend/src/main/java/net/sternstein/saft/api/ProductRestApi.java
@@ -1,4 +1,17 @@
 package net.sternstein.saft.api;
 
+import net.sternstein.saft.services.ProductService;
+
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+@Path("product")
+@Consumes("application/json")
+@Produces("application/json")
 public class ProductRestApi implements ProductApi {
+
+    @Inject
+    ProductService productService;
 }
--- src/saft-backend/src/main/java/net/sternstein/saft/api/TransactionRestApi.java
+++ src/saft-backend/src/main/java/net/sternstein/saft/api/TransactionRestApi.java
@@ -1,4 +1,17 @@
 package net.sternstein.saft.api;
 
+import net.sternstein.saft.services.TransactionService;
+
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+@Path("transaction")
+@Consumes("application/json")
+@Produces("application/json")
 public class TransactionRestApi implements TransactionApi {
+
+    @Inject
+    TransactionService transactionService;
 }
