29930b54fcb16b38c4a1944ee127fffa74d5bd5b
Michael Dumfart <michi.dumfart@gmail.com>
2022-07-21 22:08:15 +0200

    restructure project and readd angular

--- src/saft-ui/src/app/app.component.ts
+++ src/saft-ui/src/app/app.component.ts
@@ -3,13 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'saft-gui';
-  public isAuthenticated = false;
-
-  public logout():void{
-    
-  }
+  title = 'saft-ui';
 }
