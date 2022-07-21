7f9b6690040fd6202bb2ec92155f85d7a849389b
Michael Dumfart <michi.dumfart@gmail.com>
2022-07-21 22:36:47 +0200

    Readd base login

--- src/saft-ui/src/app/app-routing.module.ts
+++ src/saft-ui/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginModule } from './login/login.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  {
+    path: 'login',
+    loadChildren: () =>
+      import('./login/login.module').then((m) => m.LoginModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/saft-ui/src/app/shared/shared.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, MatCardModule],
+  exports: [MatCardModule],
+})
+export class SharedModule {}
