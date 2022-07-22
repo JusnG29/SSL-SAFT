54b9f638415fae223d9bc93628edfe27395ec752
mdumfart <michi.dumfart@gmail.com>
2022-07-22 18:53:34 +0200

    Add component base

---
+++ src/saft-personal-ui/src/app/post-login/post-login-page/post-login-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-post-login-page',
+  templateUrl: './post-login-page.component.html',
+  styleUrls: ['./post-login-page.component.scss']
+})
+export class PostLoginPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
