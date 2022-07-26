c8876c09a7ff8722fbd89543494df99b13c3df24
JusnG29 <juliangartner@gmail.com>
2022-07-26 19:46:25 +0200

    implement service-layer

--- src/saft-backend/src/main/java/net/sternstein/saft/services/ProductServiceImpl.java
+++ src/saft-backend/src/main/java/net/sternstein/saft/services/ProductServiceImpl.java
@@ -1,26 +1,48 @@
 package net.sternstein.saft.services;
 
+import net.sternstein.saft.domain.Product;
+import net.sternstein.saft.persistence.ProductRepository;
+
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import java.math.BigDecimal;
+import java.util.List;
 
 @ApplicationScoped
 public class ProductServiceImpl implements ProductService {
-    @Override
-    public void createProduct() {
 
+    @Inject
+    ProductRepository productRepository;
+
+    @Override
+    public Product createProduct(String name, BigDecimal price) {
+        var product = new Product(name, price);
+
+        productRepository.persist(product);
+
+        return product;
     }
 
     @Override
-    public void getAllProducts() {
-
+    // TODO: GAJ ID!
+    public Product getProduct(Long id) {
+        return productRepository.findById(id);
     }
 
     @Override
-    public void updateProduct() {
-
+    public List<Product> getAllProducts() {
+        return productRepository.findAll().stream().toList();
     }
 
     @Override
-    public void deleteProduct() {
+    public Product updateProduct(Product product) {
+        productRepository.persist(product);
+        return product;
+    }
 
+    @Override
+    // TODO: GAJ ID!
+    public boolean deleteProduct(Long id) {
+        return productRepository.deleteById(id);
     }
 }
