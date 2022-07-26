0c3798f3a2a3e60c943394916ca6379ae80873e5
JusnG29 <juliangartner@gmail.com>
2022-07-26 19:46:25 +0200

    implement service-layer

--- src/saft-backend/src/main/java/net/sternstein/saft/services/TransactionService.java
+++ src/saft-backend/src/main/java/net/sternstein/saft/services/TransactionService.java
@@ -1,8 +1,14 @@
 package net.sternstein.saft.services;
 
+import net.sternstein.saft.domain.Transaction;
+
+import java.math.BigDecimal;
+import java.util.List;
+
 public interface TransactionService {
-    void createTransaction();
-    void getTransaction();
-    void updateTransaction();
-    void deleteTransaction();
+    Transaction createTransaction(Long userId, Long productId, BigDecimal value, int amount);
+    Transaction getTransaction(Long id);
+    List<Transaction> getAllTransactions();
+    Transaction updateTransaction(Transaction transaction);
+    boolean deleteTransaction(Long id);
 }
