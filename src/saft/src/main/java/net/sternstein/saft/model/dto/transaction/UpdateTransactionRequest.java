7ce696ce5aaa660adbb85e00a8d5586bc57509f5
JusnG29 <juliangartner@gmail.com>
2022-07-28 23:55:10 +0200

    removed comments

--- src/saft/src/main/java/net/sternstein/saft/model/dto/transaction/UpdateTransactionRequest.java
+++ src/saft/src/main/java/net/sternstein/saft/model/dto/transaction/UpdateTransactionRequest.java
@@ -2,6 +2,5 @@ package net.sternstein.saft.model.dto.transaction;
 
 import net.sternstein.saft.domain.Transaction;
 
-// TODO: check serialization stuff
 public record UpdateTransactionRequest(Transaction transaction) {
 }
