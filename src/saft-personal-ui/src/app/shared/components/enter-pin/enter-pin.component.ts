8e86988c615e7c7e625c69be8a9c71cd718ea9b2
Michael Dumfart <michi.dumfart@gmail.com>
2022-07-28 20:17:21 +0200

    Add login user list

---
+++ src/saft-personal-ui/src/app/shared/components/enter-pin/enter-pin.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-enter-pin',
+  templateUrl: './enter-pin.component.html',
+  styleUrls: ['./enter-pin.component.scss'],
+})
+export class EnterPinComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
