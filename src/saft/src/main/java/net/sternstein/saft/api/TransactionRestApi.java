987f41e94ccd17f7ae2a941b3c345a60dd535403
JusnG29 <juliangartner@gmail.com>
2022-07-29 00:10:42 +0200

    add annotations for open-api

--- src/saft/src/main/java/net/sternstein/saft/api/TransactionRestApi.java
+++ src/saft/src/main/java/net/sternstein/saft/api/TransactionRestApi.java
@@ -1,14 +1,22 @@
 package net.sternstein.saft.api;
 
+import net.sternstein.saft.domain.Transaction;
 import net.sternstein.saft.model.dto.transaction.CreateTransactionRequest;
 import net.sternstein.saft.model.dto.transaction.GetUserHistoryRequest;
 import net.sternstein.saft.model.dto.transaction.PurchaseRequest;
 import net.sternstein.saft.model.dto.transaction.UpdateTransactionRequest;
 import net.sternstein.saft.service.TransactionService;
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.enums.SchemaType;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
 
 import javax.inject.Inject;
 import javax.transaction.Transactional;
 import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.UUID;
 
@@ -22,6 +30,16 @@ public class TransactionRestApi implements TransactionApi {
 
     @POST
     @Transactional
+    @Operation(summary = "Create transaction", operationId = "createTransaction")
+    @APIResponses({
+            @APIResponse(
+                    name = "Success",
+                    description = "Transaction created and returned",
+                    content = @Content(
+                            schema = @Schema(type = SchemaType.OBJECT, implementation = Transaction.class),
+                            mediaType = MediaType.APPLICATION_JSON),
+                    responseCode = "200"
+            )})
     @Override
     public Response createTransaction(CreateTransactionRequest request) {
         var transaction = transactionService.createTransaction(request.userId(), request.productId(), request.value(), request.amount());
@@ -30,6 +48,16 @@ public class TransactionRestApi implements TransactionApi {
 
     @GET
     @Path("{id}")
+    @Operation(summary = "Get Transaction by ID", operationId = "getTransaction")
+    @APIResponses({
+            @APIResponse(
+                    name = "Success",
+                    description = "Transaction found and returned",
+                    content = @Content(
+                            schema = @Schema(type = SchemaType.OBJECT, implementation = Transaction.class),
+                            mediaType = MediaType.APPLICATION_JSON),
+                    responseCode = "200"
+            )})
     @Override
     public Response getTransaction(UUID id) {
         var transaction = transactionService.getTransaction(id);
@@ -38,6 +66,16 @@ public class TransactionRestApi implements TransactionApi {
 
     @GET
     @Path("all")
+    @Operation(summary = "Get all transactions", operationId = "getAllTransactions")
+    @APIResponses({
+            @APIResponse(
+                    name = "Success",
+                    description = "All transactions returned",
+                    content = @Content(
+                            schema = @Schema(type = SchemaType.ARRAY, implementation = Transaction.class),
+                            mediaType = MediaType.APPLICATION_JSON),
+                    responseCode = "200"
+            )})
     @Override
     public Response getAllTransactions() {
         var transactions = transactionService.getAllTransactions();
@@ -46,6 +84,16 @@ public class TransactionRestApi implements TransactionApi {
 
     @PUT
     @Transactional
+    @Operation(summary = "Update Transaction", operationId = "updateTransaction")
+    @APIResponses({
+            @APIResponse(
+                    name = "Success",
+                    description = "Transaction updated",
+                    content = @Content(
+                            schema = @Schema(type = SchemaType.OBJECT, implementation = Transaction.class),
+                            mediaType = MediaType.APPLICATION_JSON),
+                    responseCode = "200"
+            )})
     @Override
     public Response updateTransaction(UpdateTransactionRequest request) {
         var transaction = transactionService.updateTransaction(request.transaction());
@@ -55,6 +103,16 @@ public class TransactionRestApi implements TransactionApi {
     @DELETE
     @Path("{id}")
     @Transactional
+    @Operation(summary = "Delete Transaction by ID", operationId = "deleteTransaction")
+    @APIResponses({
+            @APIResponse(
+                    name = "Success",
+                    description = "Transaction deleted",
+                    content = @Content(
+                            schema = @Schema(type = SchemaType.DEFAULT),
+                            mediaType = MediaType.APPLICATION_JSON),
+                    responseCode = "200"
+            )})
     @Override
     public Response deleteTransaction(UUID id) {
         transactionService.deleteTransaction(id);
@@ -64,6 +122,16 @@ public class TransactionRestApi implements TransactionApi {
     @POST
     @Path("purchase")
     @Transactional
+    @Operation(summary = "User purchases a product", operationId = "purchase")
+    @APIResponses({
+            @APIResponse(
+                    name = "Success",
+                    description = "Product purchased and transaction created",
+                    content = @Content(
+                            schema = @Schema(type = SchemaType.OBJECT, implementation = Transaction.class),
+                            mediaType = MediaType.APPLICATION_JSON),
+                    responseCode = "200"
+            )})
     @Override
     public Response purchase(PurchaseRequest request) {
         var transaction = transactionService.purchase(request.userId(), request.productId(), request.value(), request.amount());
@@ -72,6 +140,16 @@ public class TransactionRestApi implements TransactionApi {
 
     @POST
     @Path("getUserHistory")
+    @Operation(summary = "Get transaction-history for user by ID", operationId = "getUserHistory")
+    @APIResponses({
+            @APIResponse(
+                    name = "Success",
+                    description = "Transaction-history for user returned",
+                    content = @Content(
+                            schema = @Schema(type = SchemaType.ARRAY, implementation = Transaction.class),
+                            mediaType = MediaType.APPLICATION_JSON),
+                    responseCode = "200"
+            )})
     @Override
     public Response getUserHistory(GetUserHistoryRequest request) {
         var userHistory = transactionService.getUserHistory(request.userId());
--- src/saft/src/main/java/net/sternstein/saft/api/UserRestApi.java
+++ src/saft/src/main/java/net/sternstein/saft/api/UserRestApi.java
@@ -1,15 +1,24 @@
 package net.sternstein.saft.api;
 
+import net.sternstein.saft.domain.User;
 import net.sternstein.saft.model.dto.user.BalanceRequest;
 import net.sternstein.saft.model.dto.user.CreateUserRequest;
 import net.sternstein.saft.model.dto.user.LoginRequest;
 import net.sternstein.saft.model.dto.user.UpdateUserRequest;
 import net.sternstein.saft.service.UserService;
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.enums.SchemaType;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
 
 import javax.inject.Inject;
 import javax.transaction.Transactional;
 import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.math.BigDecimal;
 import java.util.UUID;
 
 @Path("user")
@@ -21,6 +30,16 @@ public class UserRestApi implements UserApi {
 
     @POST
     @Transactional
+    @Operation(summary = "Create user", operationId = "createUser")
+    @APIResponses({
+            @APIResponse(
+                    name = "Success",
+                    description = "User was created and returned",
+                    content = @Content(
+                            schema = @Schema(type = SchemaType.OBJECT, implementation = User.class),
+                            mediaType = MediaType.APPLICATION_JSON),
+                    responseCode = "200"
+            )})
     @Override
     public Response createUser(CreateUserRequest request) {
         // TODO: do some validation
@@ -32,6 +51,16 @@ public class UserRestApi implements UserApi {
 
     @GET
     @Path("{id}")
+    @Operation(summary = "Get user by ID", operationId = "getUser")
+    @APIResponses({
+            @APIResponse(
+                    name = "Success",
+                    description = "User was found and returned",
+                    content = @Content(
+                            schema = @Schema(type = SchemaType.OBJECT, implementation = User.class),
+                            mediaType = MediaType.APPLICATION_JSON),
+                    responseCode = "200"
+            )})
     @Override
     public Response getUser(UUID id) {
         var user = userService.getUser(id);
@@ -40,6 +69,16 @@ public class UserRestApi implements UserApi {
 
     @GET
     @Path("all")
+    @Operation(summary = "Get all users", operationId = "getAllUsers")
+    @APIResponses({
+            @APIResponse(
+                    name = "Success",
+                    description = "All users returned",
+                    content = @Content(
+                            schema = @Schema(type = SchemaType.ARRAY, implementation = User.class),
+                            mediaType = MediaType.APPLICATION_JSON),
+                    responseCode = "200"
+            )})
     @Override
     public Response getAllUsers() {
         var users = userService.getAllUsers();
@@ -49,6 +88,16 @@ public class UserRestApi implements UserApi {
 
     @PUT
     @Transactional
+    @Operation(summary = "Update user", operationId = "updateUser")
+    @APIResponses({
+            @APIResponse(
+                    name = "Success",
+                    description = "User was updated",
+                    content = @Content(
+                            schema = @Schema(type = SchemaType.OBJECT, implementation = User.class),
+                            mediaType = MediaType.APPLICATION_JSON),
+                    responseCode = "200"
+            )})
     @Override
     public Response updateUser(UpdateUserRequest request) {
         var user = userService.updateUser(request.user());
@@ -58,6 +107,16 @@ public class UserRestApi implements UserApi {
     @DELETE
     @Path("{id}")
     @Transactional
+    @Operation(summary = "Delete user by ID", operationId = "deleteUser")
+    @APIResponses({
+            @APIResponse(
+                    name = "Success",
+                    description = "User was deleted",
+                    content = @Content(
+                            schema = @Schema(type = SchemaType.DEFAULT),
+                            mediaType = MediaType.APPLICATION_JSON),
+                    responseCode = "200"
+            )})
     @Override
     public Response deleteUser(UUID id) {
         userService.deleteUser(id);
@@ -66,6 +125,16 @@ public class UserRestApi implements UserApi {
 
     @POST
     @Path("login")
+    @Operation(summary = "Login for user", operationId = "login")
+    @APIResponses({
+            @APIResponse(
+                    name = "Success",
+                    description = "User was logged in",
+                    content = @Content(
+                            schema = @Schema(type = SchemaType.DEFAULT),
+                            mediaType = MediaType.APPLICATION_JSON),
+                    responseCode = "200"
+            )})
     @Override
     public Response login(LoginRequest request) {
         boolean success = userService.login(request.id(), request.passcode());
@@ -78,6 +147,16 @@ public class UserRestApi implements UserApi {
 
     @POST
     @Path("balance")
+    @Operation(summary = "Get balance for user by ID", operationId = "balance")
+    @APIResponses({
+            @APIResponse(
+                    name = "Success",
+                    description = "User was found and balance returned",
+                    content = @Content(
+                            schema = @Schema(type = SchemaType.NUMBER, implementation = BigDecimal.class),
+                            mediaType = MediaType.APPLICATION_JSON),
+                    responseCode = "200"
+            )})
     @Override
     public Response balance(BalanceRequest request) {
         var balance = userService.getBalance(request.id());
