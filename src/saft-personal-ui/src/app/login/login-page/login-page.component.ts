622b5a445d5e83b1bcfde490e6beb2948b484860
Michael Dumfart <michi.dumfart@gmail.com>
2022-07-28 20:37:16 +0200

    Add pin dialog

--- src/saft-personal-ui/src/app/login/login-page/login-page.component.ts
+++ src/saft-personal-ui/src/app/login/login-page/login-page.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { NavController } from '@ionic/angular';
+import { MatDialog } from '@angular/material/dialog';
 import { Subject, takeUntil } from 'rxjs';
+import { EnterPinComponent } from '../../shared/components/enter-pin/enter-pin.component';
 import { User } from '../../shared/domain/user';
 import { UserService } from '../../shared/services/user.service';
 
@@ -11,21 +11,14 @@ import { UserService } from '../../shared/services/user.service';
   styleUrls: ['./login-page.component.scss'],
 })
 export class LoginPageComponent implements OnInit, OnDestroy {
-  public loginForm: FormGroup;
   public allUsers: User[] = [];
 
   private $end: Subject<void> = new Subject();
 
   constructor(
-    private readonly formBuilder: FormBuilder,
     private readonly userService: UserService,
-    private readonly navController: NavController
-  ) {
-    this.loginForm = formBuilder.group({
-      couleurName: [undefined, Validators.compose([Validators.required])],
-      passCode: [undefined, Validators.compose([Validators.required])],
-    });
-  }
+    private readonly pinDialog: MatDialog
+  ) {}
 
   ngOnInit(): void {
     this.initializeUserList();
@@ -36,27 +29,31 @@ export class LoginPageComponent implements OnInit, OnDestroy {
     this.$end.complete();
   }
 
-  test() {
-    console.log(this.allUsers);
+  public openPinDialog(user: User): void {
+    const pinDialogRef = this.pinDialog.open(EnterPinComponent);
+
+    pinDialogRef.afterClosed().subscribe((result) => {
+      console.log(result);
+    });
   }
 
-  public initializeLogin() {
-    if (this.loginForm.valid) {
-      this.userService
-        .login(
-          this.loginForm.get('couleurName')?.value,
-          this.loginForm.get('passCode')?.value
-        )
-        .subscribe({
-          next: (user) => {
-            this.navController.navigateRoot('/home');
-            this.userService.setAuthenticatedUser(user);
-          },
-          error: (error) => {
-            console.error(error);
-          },
-        });
-    }
+  private initializeLogin() {
+    // if (this.loginForm.valid) {
+    //   this.userService
+    //     .login(
+    //       this.loginForm.get('couleurName')?.value,
+    //       this.loginForm.get('passCode')?.value
+    //     )
+    //     .subscribe({
+    //       next: (user) => {
+    //         this.navController.navigateRoot('/home');
+    //         this.userService.setAuthenticatedUser(user);
+    //       },
+    //       error: (error) => {
+    //         console.error(error);
+    //       },
+    //     });
+    // }
   }
 
   // TODO: Add error handling
