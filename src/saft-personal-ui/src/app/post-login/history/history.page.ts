e017878d1c2c754aed7b6a5ec9ce94ae3c12f330
Michael Dumfart <michi.dumfart@gmail.com>
2022-07-29 12:50:07 +0200

    Add history page logic and card

--- src/saft-personal-ui/src/app/post-login/history/history.page.ts
+++ src/saft-personal-ui/src/app/post-login/history/history.page.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Observable, Subject, takeUntil } from 'rxjs';
+import { Transaction } from '../../openapi-generated/models';
+import { TransactionService } from '../../shared/services/transaction.service';
+import { UserService } from '../../shared/services/user.service';
 
 @Component({
   selector: 'app-history',
   templateUrl: 'history.page.html',
   styleUrls: ['history.page.scss'],
 })
-export class HistoryPage {
-  constructor() {}
+export class HistoryPage implements OnInit, OnDestroy {
+  public history: Transaction[] = [];
+  private $end: Subject<void> = new Subject();
+
+  constructor(
+    private readonly userService: UserService,
+    private readonly transactionService: TransactionService
+  ) {}
+
+  ngOnInit(): void {
+    const user = this.userService.getAuthenticatedUser();
+
+    if (user) {
+      this.transactionService
+        .getUserHistory(user.id)
+        .pipe(takeUntil(this.$end))
+        .subscribe({ next: (transactions) => (this.history = transactions) });
+    }
+  }
+
+  ngOnDestroy(): void {
+    throw new Error('Method not implemented.');
+  }
 }
