27a032deee00861e1fc174a32d29e56f43b50a47
Michael Dumfart <michi.dumfart@gmail.com>
2022-07-30 20:23:36 +0200

    Implement logged in guard

--- src/saft-personal-ui/src/app/app.component.ts
+++ src/saft-personal-ui/src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { StorageService } from './shared/services/storage.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit {
+  constructor(private readonly storageService: StorageService) {}
+
+  async ngOnInit() {
+    await this.storageService.initialize();
+  }
 }
